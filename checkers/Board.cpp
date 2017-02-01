4cd0fe08f40008cf1b4f2c8fd4c20b8bc9aba360
GrachevaElena <alyona-gra98@yandex.ru>
2017-02-01 15:01:34 +0300

    Some additios to class Board

--- checkers/Board.cpp
+++ checkers/Board.cpp
@@ -1,13 +1,4 @@
 #include "Board.h"
-
-Board::Board(ListOfCheckers & white, ListOfCheckers & black)
-{
-	for (auto it = white.begin(); it; it++)
-		board[(*it).GetCoord()] = &(*it);
-	for (auto it = black.begin(); it; it++)
-		board[(*it).GetCoord()] = &(*it);
-}
-
 void Board::Set(ListOfCheckers & white, ListOfCheckers & black)
 {
 	for (int i = 0; i < 64; i++) board[i] = 0;
--- include/Board.h
+++ include/Board.h
@@ -4,7 +4,7 @@ class Board {
 	Checker* board[64];
 public:
 	Board() { for (int i = 0; i < 64; i++) board[i] = 0; }
-	Board(ListOfCheckers& white, ListOfCheckers& black);
+	Board(ListOfCheckers& white, ListOfCheckers& black) { Set(white, black); }
 	void Set(ListOfCheckers& white, ListOfCheckers& black);
 	int IsEmpty(int cell) { return board[cell] == 0; }
 	Checker& operator [](int cell) { if (IsEmpty(cell)) { throw 1; return Checker(); } return *(board[cell]); }
