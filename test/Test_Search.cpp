ee9b7f510279f245918d2f2d1a9c229f90d9cbfc
GrachevaElena <alyona-gra98@yandex.ru>
2017-04-29 19:10:32 +0300

    Smart Evaluate

--- test/Test_Search.cpp
+++ test/Test_Search.cpp
@@ -45,3 +45,23 @@ TEST(test_AlphaBetaSearch_Smart, can_evaluate_normally) {
 
 	board.Clean();
 }
+
+TEST(test_AlphaBetaSearch, end_of_game) {
+	const int nw = 6, nb = 1;
+	int typesw[nw] = { 1,1,1,1,1,1 };
+	int typesb[nb] = { 0 };
+	int coordsw[nw] = { 10,12,26,44,46,60 };
+	int coordsb[nb] = { 42 };
+
+	checkers[0].GenerateInitialPosition(0, typesw, coordsw, nw);
+	checkers[1].GenerateInitialPosition(1, typesb, coordsb, nb);
+
+	board.Set(checkers[0], checkers[1]);
+
+	Move BestMove, s;
+	SearchAlphaBeta(1, 2, -INF, INF, &BestMove);
+
+	EXPECT_TRUE(BestMove != s);
+
+	board.Clean();
+}
