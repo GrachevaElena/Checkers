c7a62487b23a21a65d28ba88b65daba6378534f4
GrachevaElena <alyona-gra98@yandex.ru>
2017-05-09 19:09:57 +0300

    bit version of classes

--- checkers/Move.cpp
+++ checkers/Move.cpp
@@ -2,31 +2,27 @@
 
 int operator==(const Move & m1, const Move & m2)
 {
-	if (m1.Color != m2.Color) return 0;
-	if (m1.Type != m2.Type) return 0;
-	if (m1.Num != m2.Num) return 0;
-	if (m1.StartCoord != m2.StartCoord) return 0;
-	if (m1.FinalCoord != m2.FinalCoord) return 0;
-	if (m1.NEaten != m2.NEaten) return 0;
-	int f = 0;
-	for (int i = 0; i < m1.NEaten; i++) {
+	if ((m1.move<<42) != (m2.move << 42)) return 0;
+	int f, n = ((m1.move >> OFFSET_NEATEN) & MASK_NEATEN); long long coord;
+	for (int i = 0; i < n; i++) {
 		f = 0;
-		for (int j=0; j<m2.NEaten; j++)
-			if (m1.Eaten[i] == m2.Eaten[i]) f=1;
-		if (f = 0) return 0;
+		coord = (m1.move >> (OFFSET_EATEN + i * 4))&MASK_NEATEN;
+		for (int j = 0; j < n; j++)
+			if (coord == ((m2.move >> (OFFSET_EATEN + j * 4))&MASK_NEATEN)) {
+				f = 1;
+				break;
+			}
+		if (!f) return 0;
 	}
 	return 1;
 }
 
-int operator!=(const Move & m1, const Move & m2) {
-	return !(m1 == m2);
-}
-
-void Move::SetVarEaten(unsigned long long n1, ...) {
-	unsigned long long* p = &n1;
-	int n = 0;
-	for (; (p[n]) && (n<MaxNEaten); n++) {
-		Eaten[n] = p[n];
+void Move::SetVarEaten(ull n1, ...) {
+	ull * p = &n1;
+	int n=0;
+	move = move&INVMASK_EATEN&INVMASK_NEATEN;
+	for (; (*p)&&(n<10); n++, p++) {
+		move = move|(*p << (n * 4 + OFFSET_EATEN));
 	}
 	SetNEaten(n);
 }
--- include/Move.h
+++ include/Move.h
@@ -2,52 +2,72 @@
 
 // _ _ | 62 _ _ _ _ ... _ _ _ _ (съеденные шашки (10)) 22_ _ _ _ (количество съеденных шашек) 18_ _ _  _ _ _ (конечные координаты) 12_ _ _  _ _ _ (начальные координаты) 6_ _ _ _ (номер шашки)  2_ (изменился ли тип) 1_ (цвет шашки, не обязательно)
 
+typedef unsigned long long ull;
 #define BLACK 1
 #define WHITE 0
 
 #define DAMKA 1
 #define CHECKER 0
 
-const int MaxNEaten = 12;
+
+#define OFFSET_COLOR 0
+#define OFFSET_TYPE 1
+#define OFFSET_NUM 2
+#define OFFSET_COORD_S 6
+#define OFFSET_COORD_F 12
+#define OFFSET_NEATEN 18
+#define OFFSET_EATEN 22
+
+#define MASK_COLOR 1
+#define MASK_TYPE 1
+#define MASK_NUM 15
+#define MASK_COORD_F 63
+#define MASK_COORD_S 63
+#define MASK_NEATEN 15
+#define MASK_EATEN (ull)1099511627775
+
+#define INVMASK_COLOR ~(MASK_COLOR<<OFFSET_COLOR)
+#define INVMASK_TYPE ~(MASK_TYPE<<OFFSET_TYPE)
+#define INVMASK_NUM ~(MASK_NUM<<OFFSET_NUM)
+#define INVMASK_COORD_S ~(MASK_COORD_S<<OFFSET_COORD_S)
+#define INVMASK_COORD_F ~(MASK_COORD_F<<OFFSET_COORD_F)
+#define INVMASK_NEATEN ~(MASK_NEATEN<<OFFSET_NEATEN)
+#define INVMASK_EATEN ((ull)~(MASK_EATEN<<OFFSET_EATEN))
+
+const int MaxEaten = 10;
 
 class Move {
-	int Color;
-	int StartCoord;
-	int FinalCoord;
-	int Num;
-	int Type;
-	int NEaten;
-	int Eaten[MaxNEaten] = { 0 };
+	ull move;
 public:
-	Move() :Color(0), Type(0), StartCoord(0), FinalCoord(0), Num(0), NEaten(0) {}
+	Move() { move = 0; }
+	Move(int n) { move = n; }
+	operator long long() { return move; }
 	friend int operator==(const Move& m1, const Move& m2);
-	friend int operator!=(const Move& m1, const Move& m2);
 	~Move() {}
 
 	void Set(int _color, int _s_coord, int _f_coord, int _num, int _type) { SetColor(_color); SetStartCoord(_s_coord); SetFinalCoord(_f_coord); SetNum(_num); SetType(_type); }
-	void SetVarEaten(unsigned long long n1, ...);
+	void SetVarEaten(ull n1,...);
 
-	void SetColor(int color) { Color = color; }
-	void SetStartCoord(int coord) { StartCoord = coord; }
-	void SetFinalCoord(int coord) { FinalCoord = coord;; }
-	void SetNum(int num) { Num = num; }
-	void SetType(int type) { Type = type; }
-	void SetNEaten(int neaten) { NEaten = neaten; }
-	void SetEaten(int* arrEaten) {
-		for (int i = 0; i < GetNEaten(); i++)
-			Eaten[i] = arrEaten[i];
-	}
+	void SetColor(ull color) { move = move & INVMASK_COLOR | (color << OFFSET_COLOR); }
+	void SetStartCoord(ull coord) { move = move & INVMASK_COORD_S | (coord << OFFSET_COORD_S); }
+	void SetFinalCoord(ull coord) { move = move & INVMASK_COORD_F | (coord << OFFSET_COORD_F); }
+	void SetNum(ull num) { move = move & INVMASK_NUM | (num << OFFSET_NUM); }
+	void SetType(ull type) { move = move & INVMASK_TYPE | (type << OFFSET_TYPE); }
+	void SetNEaten(ull neaten) { move = move & INVMASK_NEATEN | (neaten << OFFSET_NEATEN); }
+	void SetEaten(ull eaten) { move = move & INVMASK_EATEN | (eaten << OFFSET_EATEN); }
+	void SetEaten(int* arrEaten) 
+	{ move = move & INVMASK_EATEN; 
+	for (int i = 0; i < GetNEaten(); i++) 
+		move = move | ((ull)arrEaten[i] << (OFFSET_EATEN + 4 * i)); }
 
-	int GetColor() { return Color; }
-	int GetStartCoord() { return StartCoord; }
-	int GetFinalCoord() { return FinalCoord; }
-	int GetNum() { return Num; }
-	int GetType() { return Type; }
-	int GetNEaten() { return NEaten; }
-	void GetEaten(int* arrEaten)
-	{
-		for (int i = 0; i < MaxNEaten; i++)
-			arrEaten[i] = Eaten[i];
-	}
+	int GetColor() { return move & MASK_COLOR; }
+	int GetStartCoord() { return (move >> OFFSET_COORD_S) & MASK_COORD_S; }
+	int GetFinalCoord() { return (move >> OFFSET_COORD_F) & MASK_COORD_F; }
+	int GetNum() { return (move >> OFFSET_NUM) & MASK_NUM; }
+	int GetType() { return (move >> OFFSET_TYPE) & MASK_TYPE; }
+	int GetNEaten() { return (move >> OFFSET_NEATEN) & MASK_NEATEN; }
+	long long GetEaten() { return (move >> OFFSET_EATEN) & MASK_EATEN; }
+	void GetEaten(int* arrEaten) 
+	{ for (int i = 0; i < 10; i++) 
+		arrEaten[i] = (move >> (OFFSET_EATEN + i * 4)) & MASK_NUM; }
 };
-
